56a7f7d26a716a9b23ee0bb0e7d723b005b3d34d
LanguageXange <hlin0017@seattlecentral.edu>
2021-09-30 10:44:33 -0700

    redux typescript set up

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class App extends React.Component {
+  render() {
+    return <div>TS redux app!</div>;
+  }
+}
---
+++ src/reducers/index.ts
@@ -0,0 +1,7 @@
+// this file is simply a ts file we are not using any JSX syntax
+
+import { combineReducers } from "redux";
+
+export const rootReducers = combineReducers({
+  counter: () => 1,
+});
